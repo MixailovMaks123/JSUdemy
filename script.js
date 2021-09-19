0e570da9a242bd346408a0d053e3d2f79612d340
Maksim <mixailovmaks123@gmail.com>
2021-09-19 23:24:56 +0900

    exercise3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money = +prompt("Ваш бюджет на месяц?", ''),
+	time = +prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
 	budget: money,
@@ -12,16 +12,30 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a2 = prompt("Во сколько обойдется?", ''),
-	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+for(let i=0; i<2; i++){
+	let a = +prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = +prompt("Во сколько обойдется?", '');
 
-// Важно! В последних версиях браузеров поведение работы со свойствами объектов поменялось. 
-// Теперь синтаксис через точку может не работать. Поэтому выше я использовал квадратные скобки. 
-// Если вы это читаете - то в скором времени обновится и видео в самом курсе. Спасибо за внимание!
+	if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b !='' && a.length < 50){
+		
+		appData.expenses[a]	= b;
+	}else{
+		i = i - 1;
+	}
+	
+};
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appDay.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100){
+	console.log('Минтмальный уровень достатка');
+}else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+	console.log('Средний уровень достатка');
+}else if(appData.moneyPerDay > 2000){
+	console.log('Высокий уровень достатка');
+}else{
+	console.log('Error');
+}
