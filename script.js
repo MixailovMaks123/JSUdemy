3ccd98ee49f90140fd59c6fafc7eca304b995abe
Maksim <mixailovmaks123@gmail.com>
2021-09-19 23:37:49 +0900

    exprecise3

--- script.js
+++ script.js
@@ -39,3 +39,37 @@ if(appData.moneyPerDay < 100){
 }else{
 	console.log('Error');
 }
+
+
+// 1.
+// let i = 0;
+// while (i < 2) {
+// let a = prompt("Введите обязательную статью расходов в этом месяце ", "Еда"),
+// b = prompt("Во сколько обойдется? ", "10000");
+// i++;
+//     if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) === "string" && (typeof(b)) != null 
+//     && a != '' && b != '' && a.length < 50)  {
+//         console.log("done"); 
+//         appData.expenses[a] = b;
+//     } else {
+//         i = i - 1;
+//         continue;
+//     }
+// }
+
+// 2.
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце ", "Еда"),
+//     b = prompt("Во сколько обойдется? ", "10000");
+//     i++;
+//     if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) === "string" && (typeof(b)) != null 
+//     && a != '' && b != '' && a.length < 50)  {
+//         console.log("done"); 
+//         appData.expenses[a] = b;
+//     } else {
+//         i = i - 1;
+//         continue;
+//     }
+// }
+// while (i < 2);
