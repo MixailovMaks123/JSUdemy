4d0ae87846ea05b749348a302a326477c6b2a1c5
Maksim <mixailovmaks123@gmail.com>
2021-09-19 23:47:52 +0900

    exprecise3

--- script.js
+++ script.js
@@ -44,32 +44,26 @@ if(appData.moneyPerDay < 100){
 // 1.
 // let i = 0;
 // while (i < 2) {
-// let a = prompt("Введите обязательную статью расходов в этом месяце ", "Еда"),
-// b = prompt("Во сколько обойдется? ", "10000");
-// i++;
-//     if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) === "string" && (typeof(b)) != null 
-//     && a != '' && b != '' && a.length < 50)  {
-//         console.log("done"); 
-//         appData.expenses[a] = b;
-//     } else {
-//         i = i - 1;
-//         continue;
-//     }
-// }
+// 		let a = +prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 			b = +prompt("Во сколько обойдется?", '');
+// 		i++;
+// 		if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b !='' && a.length < 50){
+// 		appData.expenses[a]	= b;
+// 		}else{
+// 		i = i - 1;
+// 		}
+//}
 
 // 2.
 // let i = 0;
 // do {
-//     let a = prompt("Введите обязательную статью расходов в этом месяце ", "Еда"),
-//     b = prompt("Во сколько обойдется? ", "10000");
-//     i++;
-//     if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) === "string" && (typeof(b)) != null 
-//     && a != '' && b != '' && a.length < 50)  {
-//         console.log("done"); 
-//         appData.expenses[a] = b;
-//     } else {
-//         i = i - 1;
-//         continue;
-//     }
+//      gilet a = +prompt("Введите обязательную статью расходов в этом месяце", ''),
+// 		   b = +prompt("Во сколько обойдется?", '');
+// 		i++;
+// 		if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b !='' && a.length < 50){
+// 		appData.expenses[a]	= b;
+// 		}else{
+// 		i = i - 1;
+// 		}
 // }
 // while (i < 2);
